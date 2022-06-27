eebba1f97fb40aafb7c1e5d9ba2cf46456784524
oshadaera68 <mindartlk010@gmail.com>
2022-06-27 11:04:22 +0530

    updated all contents

--- src/dao/custom/CrudUtil.java
+++ src/dao/custom/CrudUtil.java
@@ -5,13 +5,15 @@
 
 package dao.custom;
 
+import db.DbConnection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CrudUtil {
     private static PreparedStatement getPreparedStatement(String sql, Object... args) throws SQLException,ClassNotFoundException{
-        PreparedStatement pst = DB.db().getConnection().prepareStatement(sql);
+        PreparedStatement pst = DbConnection.getInstance().getConnection().prepareStatement(sql);
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
             pst.setObject(i + 1, args[i]);
--- src/dao/custom/impl/OrderDAOImpl.java
+++ src/dao/custom/impl/OrderDAOImpl.java
@@ -5,5 +5,46 @@
 
 package dao.custom.impl;
 
-public class OrderDAOImpl {
+import dao.custom.CrudDAO;
+import dao.custom.CrudUtil;
+import entity.Orders;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OrderDAOImpl implements CrudDAO<Orders,String> {
+    @Override
+    public JsonArray getAll() throws SQLException, ClassNotFoundException {
+        ResultSet rst = CrudUtil.executeQuery("SELECT * FROM orders");
+        JsonArrayBuilder orderArray = Json.createArrayBuilder();
+        JsonObjectBuilder orderJSON = Json.createObjectBuilder();
+        while (rst.next()) {
+            Orders order = new Orders(rst.getString(1),rst.getString(2),rst.getString(3),rst.getDouble(4));
+            orderJSON.add("orderId", order.getOrderId());
+            orderJSON.add("orderDate", order.getOrderDate());
+            orderJSON.add("customerId", order.getCustomerId());
+            orderJSON.add("cost", order.getCost());
+            orderArray.add(orderJSON.build());
+        }
+        return orderArray.build();
+    }
+
+    @Override
+    public boolean add(Orders orders) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Orders orders) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
 }
